55a17cf8a056b8867a1b12dfcd21e6dc3ed6a81d
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-09 09:20:50 -0400

    a

---
+++ dictunarlyiy.py
@@ -0,0 +1,11 @@
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+
+    "name": "TCL 67\" 4K UHD TV"
+    "price": 670.67,
+    "department": "Televisions",
+    "description": "67-inch Ultra HD Smart TV with streaming apps."
+}
