9947aa6b6e9f855ec34b0e8d2c2925d10cb9ffb1
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-16 08:46:52 -0400

    Update dictunarlyiy.py

--- dictunarlyiy.py
+++ dictunarlyiy.py
@@ -3,9 +3,6 @@ item = {
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-
-    "name": "TCL 67\" 4K UHD TV"
-    "price": 670.67,
-    "department": "Televisions",
-    "description": "67-inch Ultra HD Smart TV Featreing streaming apps."
 }
+
+item[0]["name"]
