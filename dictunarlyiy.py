6c0ec9a4b537ce03e217c0d30c0b9e34f157b20a
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-16 09:12:07 -0400

    a

--- dictunarlyiy.py
+++ dictunarlyiy.py
@@ -1,8 +1,17 @@
-item = {
+items = [{
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-}
+},
+{
+    "name": "TCL 60\" 4K UHD TV",
+    "price": 607.99,
+    "department": "Televisions",
+    "description": "60-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}]
 
-item[0]["name"]
+print (items[0]["description"])
+
+for index, item in enumerate(items):
+    print(index, ":", item["name"])
---
+++ dictunary.py
@@ -0,0 +1,6 @@
+items = [{
+    "name": "Acer Nitro 15"
+    "price": "499.99"
+    "specs": "Intel Core i5 13420H, RTX 3050, 15.3 Inch 144Hz IPS display "
+},
+{}
