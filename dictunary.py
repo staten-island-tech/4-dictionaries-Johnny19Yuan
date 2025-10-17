e9cd7155713cfbb3e6b88154bc7c63d7fb1079e9
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-17 09:06:00 -0400

    Update dictunary.py

--- dictunary.py
+++ dictunary.py
@@ -1,6 +1,24 @@
-items = [{
-    "name": "Acer Nitro 15"
-    "price": "499.99"
-    "specs": "Intel Core i5 13420H, RTX 3050, 15.3 Inch 144Hz IPS display "
-},
-{}
+microcenter = [
+    {
+        "name": "Acer Nitro 15",
+        "price": "499.99",
+        "specs": "Intel Core i5 13420H, RTX 3050, 15.3 Inch 144Hz IPS display",
+    },
+    {
+        "name": "Acer Nitro 16",
+        "price": "749.99",
+        "specs": "Ryzen 5 9450, RTX 4050, 16.2 Inch 144Hz IPS display" ,
+    },
+    {
+        "name": "Lenovo Legion Pro 7",
+        "price": "1999.99",
+        "specs": "Intel Core Ultra 9 275HX, RTX 5070Ti, 16.2 Inch 144Hz OLED display",
+    },
+    {
+        "name": "MSI Titan 18 HX AI A2XWJG-412US",
+        "price": "4299.99",
+        "specs": "Intel Core Ultra 9 285HX, RTX 5090, 18 Inch UHD+ Mini-LED display", 
+    }
+]
+
+print(microcenter[0]["name"])
