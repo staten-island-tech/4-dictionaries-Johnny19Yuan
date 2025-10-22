ac0379fb699446e6d26fccaad5082edc00af4c0a
Johnny19Yuan <johnny19yuan@gmail.com>
2025-10-22 08:43:22 -0400

    Update dictunary.py

--- dictunary.py
+++ dictunary.py
@@ -1,26 +1,33 @@
-microcenter = [
-    {
-        "name": "Acer Nitro 15",
-        "price": "499.99",
-        "specs": "Intel Core i5 13420H, RTX 3050, 15.3 Inch 144Hz IPS display",
-    },
-    {
-        "name": "Acer Nitro 16",
-        "price": "749.99",
-        "specs": "Ryzen 5 9450, RTX 4050, 16.2 Inch 144Hz IPS display" ,
-    },
-    {
-        "name": "Lenovo Legion Pro 7",
-        "price": "1999.99",
-        "specs": "Intel Core Ultra 9 275HX, RTX 5070Ti, 16.2 Inch 144Hz OLED display",
-    },
-    {
-        "name": "MSI Titan 18 HX AI A2XWJG-412US",
-        "price": "4299.99",
-        "specs": "Intel Core Ultra 9 285HX, RTX 5090, 18 Inch UHD+ Mini-LED display", 
-    }
-]
-for index, item in enumerate(microcenter):
-    print(index, ":", item["name"])
-useritem = int(input("What laptop would you like? Please input a number: "))
-print (microcenter[useritem]["name"])
+microcenter = [{
+    "name": "Acer Nitro 15",
+    "price": "499.99",
+    "specs": "Intel Core i5 13420H, RTX 3050, 15.3 Inch 144Hz IPS display",
+},{
+    "name": "Acer Nitro 16",
+    "price": "749.99",
+    "specs": "Ryzen 5 9450, RTX 4050, 16.2 Inch 144Hz IPS display" ,
+},{
+    "name": "Lenovo Legion Pro 7",
+    "price": "1999.99",
+    "specs": "Intel Core Ultra 9 275HX, RTX 5070Ti, 16.2 Inch 144Hz OLED display",
+},{
+    "name": "MSI Titan 18 HX AI A2XWJG-412US",
+    "price": "4299.99",
+    "specs": "Intel Core Ultra 9 285HX, RTX 5090, 18 Inch UHD+ Mini-LED display", 
+}]
+total = 0 
+cart = []
+checkout = "N"
+while checkout == "N":
+    print("Microcenter Gaming laptops:")
+    for index, item in enumerate(microcenter):
+        print(index, ":", item["name"])
+    useritem = int(input("What laptop would you like? Please input a number: "))
+    cart.append(microcenter[useritem]["name"])
+    total += (float(microcenter[useritem]["price"]))
+    print("Your cart: ") 
+    print(cart)
+    checkout = input("Do you want to check out? Y To checkout,  N To contunue shopping: ")
+print("checkout")
+print(f"you bought: {cart}")
+print(f"Your total is ${total}")
